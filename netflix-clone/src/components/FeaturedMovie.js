a674aa84d5d8adbf0c1b0a83c9ee0c084967c511
Stefano <piero.quiroz93@gmail.com>
2021-08-18 16:45:33 -0500

    Featured Movie finished

--- netflix-clone/src/components/FeaturedMovie.js
+++ netflix-clone/src/components/FeaturedMovie.js
@@ -3,6 +3,9 @@ import './FeaturedMovie.scss';
 
 const FeaturedMovie = (props) => {
     const {item} = props;
+    const firstDate = new Date(item.data.first_air_date);
+    const genres = [];
+    item.data.genres.map(item => genres.push(item.name));
     return (
         <section className="featuredMovie" style={{
             backgroundSize: 'cover', //para cualquier dimension de la pantalla
@@ -14,9 +17,15 @@ const FeaturedMovie = (props) => {
                     <div className="featuredMovie--name">{item.data.original_name}</div>
                     <div className="featuredMovie--info">
                         <div className="featuredMovie--points">{item.data.vote_average} puntos</div>
-                        <div className="featuredMovie--year">2021</div>
-                        <div className="featuredMovie--points">{item.data.number_of_seasons} temporada{item.data.number_of_seasons > 1 ? 's' : ''}</div>
+                        <div className="featuredMovie--year">{firstDate.getFullYear()}</div>
+                        <div className="featuredMovie--seasons">{item.data.number_of_seasons} temporada{item.data.number_of_seasons > 1 ? 's' : ''}</div>
                     </div>
+                    <div className="featuredMovie--description">{item.data.overview}</div>
+                    <div className="featuredMovie--buttons">
+                        <a className="button--player" href={`/watch/${item.data.id}`}>► Ver</a>
+                        <a className="button--add" href={`/list/add/${item.data.id}`}>+ Mi Lista</a>
+                    </div>
+                    <div className="featuredMovie--genres"><strong>Géneros:</strong> {genres.join(", ")}</div>
                 </div>
             </div>
         </section>
